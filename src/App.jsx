99bc6bdfe980573f396bf0638e82ebfe69c7a534
Bryant Yao <bryantyao@gmail.com>
2017-08-04 10:56:26 -0700

    Removed Welcome message and added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { Component } from 'react';
+import Header from './Header.jsx';
 
 export default class App extends Component {
     render() {
         return (
             <div id="app">
-              <h1 id="message">Welcome!</h1>
+              <Header/>
             </div>
         );
     }
