c973c0a41777a0479b535bc86b83680d44d59fb4
Bryant Yao <bryantyao@gmail.com>
2017-08-08 00:13:14 +0000

    Finalize alpha version of Experience

--- src/content/Experience.jsx
+++ src/content/Experience.jsx
@@ -5,27 +5,36 @@ export class Experience extends Component {
   render() {
     return (
       <div className={`experience ${this.props.moreClassName || ""}`}>
-        <p>I am a software engineer with <b>over 4 years of full-stack experience developing web applications</b>.</p>
-        <p>Throughout my career, I have developed <b>user-facing features</b> and <b>backend code/infrastructure</b> using:</p>
-        <table>
-          <tr>
-            <th>Company</th>
-            <th>Front end</th>
-            <th>Back end</th>
-          </tr>
-          <tr>
-            <td>IBM</td>
-            <td>JavaScript (Dojo Toolkit)</td>
-            <td>Java</td>
-          </tr>
-          <tr>
-            <td>Health Fidelity</td>
-            <td>JavaScript (jQuery) and server-side templates (ERB)</td>
-            <td>Ruby on Rails</td>
-          </tr>
+        <p>I am a software engineer with <b>over 4 years of experience developing web applications</b>.</p>
+        <div>
+        <p>Throughout my career, I have developed both <b>user-facing and backend features</b> using:</p>
+        <table className="techStack">
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Front end</th>
+              <th>Back end</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>IBM</td>
+              <td>JavaScript (Dojo Toolkit)</td>
+              <td>Java</td>
+            </tr>
+            <tr>
+              <td>Health Fidelity</td>
+              <td>JavaScript (jQuery) and server-side templates (ERB)</td>
+              <td>Ruby on Rails</td>
+            </tr>
+            </tbody>
         </table>
-        <p>I also have some experience with <b>DevOps</b> when I wrote code to <b>build and deploy an application to the cloud</b>.</p>
-        <p>I enjoy learning new languages and software!</p>
+        </div>
+        <p>I have also done some <b>DevOps</b> work at Health Fidelity, where I used <b>Chef and Jenkins</b> to provision and deploy to application servers.</p>
+        <p>I am always eager to learn and try new technologies. Currently, I'm dabbling with React (which is what this site is built in) and Docker.</p>
+        <p><span>If you would like a copy of my resume, click </span>
+          <a href="https://github.com/bryantyao/resume/raw/master/bryantyao_resume.pdf">here</a><span>.</span></p>
+        <p></p>
       </div>
     );
   }
