6a6b2738d80c1098645001d810a4601ed878bdc0
Bryant Yao <bryantyao@gmail.com>
2017-08-08 00:06:01 +0000

    Change text to Experiences

--- src/content/Experience.jsx
+++ src/content/Experience.jsx
@@ -5,10 +5,27 @@ export class Experience extends Component {
   render() {
     return (
       <div className={`experience ${this.props.moreClassName || ""}`}>
-        <p>I am a software engineer with <b>over 4 years of full-stack software development experience</b>.</p>
-        <p>I have worked with both <b>Java and Ruby</b> applications that talked to a <b>relational database (Derby and MySQL)</b>.</p>
-        <p>I started work at <a href="#ibm">IBM</a> where we used the Dojo Toolkit to develop the front end and Java to develop the back end.</p>
-        <p>Afterwards I worked at <a href="#hf">Health Fidelity</a> where we used jQuery and server-side rendering to develop the front end and Ruby to develop the back end.</p>
+        <p>I am a software engineer with <b>over 4 years of full-stack experience developing web applications</b>.</p>
+        <p>Throughout my career, I have developed <b>user-facing features</b> and <b>backend code/infrastructure</b> using:</p>
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Front end</th>
+            <th>Back end</th>
+          </tr>
+          <tr>
+            <td>IBM</td>
+            <td>JavaScript (Dojo Toolkit)</td>
+            <td>Java</td>
+          </tr>
+          <tr>
+            <td>Health Fidelity</td>
+            <td>JavaScript (jQuery) and server-side templates (ERB)</td>
+            <td>Ruby on Rails</td>
+          </tr>
+        </table>
+        <p>I also have some experience with <b>DevOps</b> when I wrote code to <b>build and deploy an application to the cloud</b>.</p>
+        <p>I enjoy learning new languages and software!</p>
       </div>
     );
   }
