83eaf29c95db0cfc18f037710bf328a6899ca7fe
Bryant Yao <bryantyao@gmail.com>
2017-08-12 23:11:02 -0700

    Switch to a syntax highlighting library for Home

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import jQuery from "jquery";
 import Popper from 'popper.js';
 import App from './App.jsx';
+import hljs from 'highlight.js';
 window.$ = window.jQuery = jQuery;
 window.Popper = Popper;
 
+// Import CSS from Libraries
 require('bootstrap');
 require('../node_modules/bootstrap/dist/css/bootstrap.css');
 require('./css/index.css');
+
+// Initialize any JS Libraries
+hljs.initHighlightingOnLoad();
 
 ReactDOM.render(
   <App />, document.getElementById('root'));
