14eede3c8f032a496691ea8d863ebf4e0e73c47f
Bryant Yao <bryantyao@gmail.com>
2017-08-19 14:45:49 -0700

    Page header should be bigger and not bold

--- src/app/pages/layouts/Page.jsx
+++ src/app/pages/layouts/Page.jsx
@@ -6,7 +6,7 @@ export class Page extends Component {
     const { className, title, children, ...other } = this.props;
     return (
       <div className={`d-flex flex-column align-items-center h-100 w-100 pt-4 ${className || ""}`} {...other}>
-        <div className="display-4 font-weight-bold mb-4">{title}</div>
+        <div className="display-3 mb-4">{title}</div>
         {children}
       </div>
     );
