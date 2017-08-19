9ef4d355fdd0732cdf665648ce3cda2db1845eac
Bryant Yao <bryantyao@gmail.com>
2017-08-18 18:04:42 -0700

    Cleanup FullPage

--- src/app/fullpagejs/FullPage.jsx
+++ src/app/fullpagejs/FullPage.jsx
@@ -4,56 +4,14 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
 export class FullPage extends Component {
-      /*afterLoad: (anchor, index) => {
-        console.log(`Loaded: ${anchor} ${index}`);
-        $(`div[data-anchor='${anchor}']`).addClass('animated fadeIn');
-      },
-      /*onLeave: (index, nextIndex, direction) => {
-        let anchor = anchors[index];
-        console.log(`Leaving: ${anchor} ${index}`);
-        $(`div[data-anchor='${anchor}']`).removeClass('animated fadeIn');
-      }*/
   componentDidMount() {
-    const pageDOM = ReactDOM.findDOMNode(this.page);
-    const { id, className, style, children, ...other } = this.props;
+    const props = Object.assign({}, props, {
+      afterLoad: this._afterLoad.bind(this),
+      onLeave: this._onLeave.bind(this)
+    });
 
-    const props = Object.assign({
-      afterLoad: this.afterLoad.bind(this),
-      onLeave: this.onLeave.bind(this)
-    }, props);
-
-    $(pageDOM)
+    $(this.page)
       .fullpage(props);
-  }
-
-  // Note: index starts at 1
-  afterLoad(anchor, index) {
-    console.log(`After load: ${this}`);
-    const component = this[`child_${index-1}`];
-    if(!component) {
-      return;
-    }
-
-    if (typeof component.onEnter === "function") { 
-      component.onEnter();
-    }
-  }
-
-  // Note: index starts at 1
-  onLeave(index, nextIndex, direction) {
-    const leavingComponent = this[`child_${index-1}`];
-    if(leavingComponent) {
-      if (typeof leavingComponent.onLeave === "function") { 
-        leavingComponent.onLeave();
-      }
-    }
-
-    const enteringComponent = this[`child_${nextIndex-1}`];
-    if(enteringComponent) {
-      if (typeof enteringComponent.beforeEnter === "function") { 
-        enteringComponent.beforeEnter();
-      }
-    }
   }
 
   render() {
@@ -73,6 +31,37 @@ export class FullPage extends Component {
         {childrenWithRef}
       </div>
     );
+  }
+
+  //private
+
+  // Note: index starts at 1
+  _afterLoad(anchor, index) {
+    const component = this[`child_${index-1}`];
+    if(!component) {
+      return;
+    }
+
+    if (typeof component.onEnter === "function") { 
+      component.onEnter();
+    }
+  }
+
+  // Note: index starts at 1
+  _onLeave(index, nextIndex, direction) {
+    const leavingComponent = this[`child_${index-1}`];
+    if(leavingComponent) {
+      if (typeof leavingComponent.onLeave === "function") { 
+        leavingComponent.onLeave();
+      }
+    }
+
+    const enteringComponent = this[`child_${nextIndex-1}`];
+    if(enteringComponent) {
+      if (typeof enteringComponent.beforeEnter === "function") { 
+        enteringComponent.beforeEnter();
+      }
+    }
   }
 }
 
