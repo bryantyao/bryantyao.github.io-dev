bc8dfdb8227fb871cef1fe44d2ec36729f94d5f0
Bryant Yao <bryantyao@gmail.com>
2017-08-23 20:24:31 +0000

    Move css -> app/css and fix clutter in index.js and index.html

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import jQuery from "jquery";
 import Popper from 'popper.js';
 import App from 'app/App.jsx';
-import hljs from 'highlight.js';
+import 'app/css/index.css';
 // bootstrap requires a global jQuery and Popper to initialize
 window.$ = window.jQuery = jQuery;
 window.Popper = Popper;
 require('bootstrap');
-
-// Initialize any JS Libraries
-hljs.initHighlightingOnLoad();
-
-// Import CSS
-require('css/index.css');
 
 ReactDOM.render(
   <App />, document.getElementById('root'));
