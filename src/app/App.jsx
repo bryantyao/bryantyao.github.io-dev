c0684d7c8e528fd9b754d3da3a63cfc8f9f0b89c
Bryant Yao <bryantyao@gmail.com>
2017-08-23 00:45:38 +0000

    Clean up App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import { About, Experience, Home, Resume, Social } from 'app/pages.js';
-import { AppBar } from 'app/nav';
-import Grid from 'material-ui/Grid';
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
-import { blue, cyan, grey } from 'material-ui/colors';
+import { grey } from 'material-ui/colors';
 
 export default class App extends Component {
-  static get COMPONENTS() {
-    return [ Home,
-             About,
-             Experience,
-             Social ];
-  }
-
   constructor(props) {
     super(props);
     this.state = { introComplete: false };
@@ -20,26 +10,27 @@ export default class App extends Component {
   }
   
   render() {
-    const components = App.COMPONENTS.map((Component, i) => (<Component key={i} style={{height: '90%'}} />));
-    const showOrHide = this.state.introComplete ? 'animated animated-delay-1100 fadeIn' : 'd-none';
+    const { introComplete } = this.state;
+    const showOrHideSocial = introComplete ? 'animated animated-delay-1100 fadeIn' : 'd-none';
+    const showOrHideMainContent = introComplete ? 'animated-delay-200 flexGrow' : 'd-none';
 
     return (
-      <div className="app">
-        <div className="row m-0" style={{ 'min-height': '100%' }}>
+      <div className='app'>
+        <div className='row m-0 min-height-100'>
           <div className={`col d-flex flex-column justify-content-center p-0`}>
             <Home
               className='my-5'
               onAnimateComplete={this._introComplete}
               typing_interval={75}
             />
-            <Social className={showOrHide}/>
+            <Social className={showOrHideSocial}/>
           </div>
           {
-            this.state.introComplete && 
-            <div className={`col d-flex flex-column pr-0 py-3 ${this.state.introComplete ? 'animated-delay-200 flexGrow' : 'd-none'}`} style={{ 'color': 'black', 'background-color': grey[50] }}>
-              <About className=" mb-4" />
-              <Resume className=" mb-4" />
-              <Experience className="" />
+            introComplete && 
+            <div className={`col d-flex flex-column pr-0 py-3 ${showOrHideMainContent}`} style={{ 'color': 'black', 'background-color': grey[50] }}>
+              <About className=' mb-4' />
+              <Resume className=' mb-4' />
+              <Experience />
             </div>
           }
         </div>
