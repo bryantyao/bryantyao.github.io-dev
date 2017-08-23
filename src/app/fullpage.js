0217c97f956dca824ba6a1b8b4345f506b72227a
Bryant Yao <bryantyao@gmail.com>
2017-08-23 00:44:55 +0000

    Remove unused code

--- src/app/fullpage.js
+++
@@ -1,4 +0,0 @@
-export * from './fullpagejs/FullPage.jsx';
-export * from './fullpagejs/Section.jsx';
-export * from './fullpagejs/FadeInSection.jsx';
-
--- src/app/nav.js
+++
@@ -1,6 +0,0 @@
-import AppBar from './nav/AppBar.jsx';
-export * from './nav/Navbar.jsx';
-export * from './nav/NavLink.jsx';
-//export AppBar from './nav/AppBar.jsx';
-export { AppBar };
-
