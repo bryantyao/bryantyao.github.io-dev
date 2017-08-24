2a1bcf6616f4fbe5759e1afbbe3b975b3542b790
Bryant Yao <bryantyao@gmail.com>
2017-08-24 16:55:01 +0000

    Remove unused require(bootstrap) (I only use bootstrap for CSS)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import jQuery from "jquery";
-import Popper from 'popper.js';
 import App from 'app/App.jsx';
 import 'app/css/index.css';
-// bootstrap requires a global jQuery and Popper to initialize
-window.$ = window.jQuery = jQuery;
-window.Popper = Popper;
-require('bootstrap');
 
 ReactDOM.render(
   <App />, document.getElementById('root'));
