4945c7e429da6f05342ee531ddae96a9e0f6dcb2
Bryant Yao <bryantyao@gmail.com>
2017-08-24 04:21:48 +0000

    Smoothen animation by removing padding

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -16,20 +16,22 @@ export default class App extends Component {
 
     return (
       <div className='app'>
-        <div className='intro row m-0 min-height-100'>
-          <div className={`col d-flex flex-column justify-content-center p-0`}>
+        <div className='row m-0 min-height-100'>
+          <div className={`intro col d-flex flex-column justify-content-center p-0`}>
             <Home
               className='my-5'
               onAnimateComplete={this._introComplete}
-              typing_interval={75}
+              typing_interval={0}
             />
             <Social className={showOrHideSocial}/>
           </div>
           {
             introComplete && 
-            <div className={`col d-flex flex-column px-3 pt-3 ${showOrHideMainContent}`} style={{ 'color': 'black', 'background-color': grey[50] }}>
-              <About className=' mb-4' />
-              <Experience />
+            <div className={`col ${showOrHideMainContent}`} style={{ 'color': 'black', 'background-color': grey[50] }}>
+              <div className="d-flex flex-column m-3">
+                <About className=' mb-4' />
+                <Experience />
+              </div>
             </div>
           }
         </div>
