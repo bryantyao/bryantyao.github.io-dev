1af2d40b19ad5d6e967af842b26797e58aabe2c4
Bryant Yao <bryantyao@gmail.com>
2017-08-25 17:25:54 +0000

    Remove tech stack and updated some text on About

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -9,15 +9,11 @@ export class About extends Component {
         <div className="">
           <div>
             <p>
-              Hey there, I'm Bryant Yao and I am a <b>full-stack software engineer</b>. I specialize in developing web applications, both front and back end.
+              Hey there, I'm Bryant Yao and I am a <b>full-stack software engineer</b>. I specialize in developing web applications, both front end and back.
             </p>
             <p>
-              I am flexible working with any tech stack. I have used:
-              <ul>
-                  <li>Dojo Toolkit with Java</li>
-                  <li>jQuery and ERB server-side templates with Ruby on Rails</li>
-              </ul>
-              Additionally, I am currently trying out React for this website (see <a href="https://github.com/bryantyao/bryantyao.github.io-dev">source</a>).
+              I love learning new technologies and I'm flexible working with most technologies. As an example, I started my career in Java and later learned Ruby (mostly from scratch) at my second job.
+              Currently, I am trying out React for this website (see <a href="https://github.com/bryantyao/bryantyao.github.io-dev">source</a>) as well as Docker.
             </p>
             <p>
               When I'm not programming, I enjoy playing video games, going to coffee shops, and sampling new beers and whiskies.
