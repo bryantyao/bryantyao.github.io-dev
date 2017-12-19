724e529ecd0e00bbb07981fb23fe474c045a2b8b
Bryant Yao <bryantyao@gmail.com>
2017-12-19 10:53:05 +0000

    Add LearnMore for all jobs

--- src/app/pages/companies/HealthFidelity/LearnMore.jsx
+++ src/app/pages/companies/HealthFidelity/LearnMore.jsx
@@ -7,17 +7,45 @@ export class LearnMore extends Component {
       <LearnMoreBase
         {...this.props}
       >
-        <h2>Summary</h2>
+        <h2 className="my-3">Summary</h2>
         <p>
           I was a full-stack software engineer on HCC Scout, a SaaS solution for medical coders to review and code medical records. I was one of the first members on the team, helping to productionize and develop the product. Additionally, I worked on some of the backend services that the product depended on like the clinical document repository.
         </p>
-        <h2>Tech Stack</h2>
+        <h2 className="my-3">Tech Stack</h2>
         <p>
           HCC Scout was built on Ruby on Rails. It used jQuery, Bootstrap and server-side templates for the front end and MySQL for the database.
         </p>
         <p>
-          On the infrastructure side, HCC Scout was provisioned and deployed using a combination of Jenkins, Capistrano and Chef.
+          On the infrastructure side, HCC Scout was provisioned and deployed using a combination of Jenkins, Capistrano, Chef, AWS OpsWorks.
         </p>
+        <h2 className="my-3">Accomplishments</h2>
+        <ul>
+          <li>
+              Developed Chef recipes to automate the provisioning of application servers
+          </li>
+            <li>
+              Wrote scripts and embedded them in Jenkins to deploy our applications
+          </li>
+            <li>
+              Developed new features for backend applications to enhance the capabilities of HCC Scout
+            <ul>
+                <li>
+                  Example: Added an OCR workflow to our document repository for processing image documents
+              </li>
+              </ul>
+            </li>
+            <li>
+              Developed new features end to end to enhance existing user workflows and meet new business requirements
+            <ul>
+                <li>
+                  Example: Developed the ICD-10 RAPs feature to view CMS accepted ICD-10 codes
+              </li>
+              </ul>
+            </li>
+            <li>
+              Worked with client services and IT Operations to improve their ability to onboard new and support existing customers
+          </li>
+        </ul>
       </LearnMoreBase>
     );
   }
--- src/app/pages/companies/IBM/LearnMore.jsx
+++ src/app/pages/companies/IBM/LearnMore.jsx
@@ -15,6 +15,21 @@ export class LearnMore extends Component {
         <p>
           Tools Base Administration console was built using Java. The front end used Dojo Toolkit, the backend used Othe OSGi framework (similar to Spring), and the database used Apache Derby.
         </p>
+        <h2 className="my-3">Accomplishments</h2>
+        <ul>
+          <li>
+            Developed new extension points that allow other developers to extend our web interface
+          </li>
+          <li>
+            Developed and enhanced web pages that are used to create a simple, comprehensive overview of the enterprise or interface with DB2 and IMS tools on the mainframe
+          </li>
+          <li>
+            Developed and maintained SQL code that can be used to capture user input or cache information from the mainframe
+          </li>
+          <li>
+            Wrote threads responsible for synchronizing databases exceptions and reports generated from IMS tools
+          </li>
+        </ul>
       </LearnMoreBase>
     );
   }
